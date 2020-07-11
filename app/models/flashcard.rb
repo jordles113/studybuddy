a257c5af5ff25870ffa904a0ea0291960e70a362
jordles113 <jordan.t.romero@gmail.com>
2020-07-11 16:09:06 -0500

    updates search tag, adds placeholder

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,5 +1,5 @@
 class Flashcard < ApplicationRecord
-    belongs_to :deck
+    belongs_to :deck, dependent: :destroy 
     validates :name, presence: true
     validates :definition, presence: true 
 
